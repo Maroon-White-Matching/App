da930bf836f38f228398afb91bcee6b9757c33cd
Kylan Lewis <klewis2416@gmail.com>
2021-10-13 23:07:26 -0500

    comment out sqlalchemy_database_uri

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-never-know-anything' 
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:49Caolo2020@localhost/capstone'
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
