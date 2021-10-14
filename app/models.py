86336088303c92c1dc0373206730613d654db9a2
Kylan Lewis <klewis2416@gmail.com>
2021-10-13 23:03:12 -0500

    Transfer
    This code is pulled from the Capstone2 repository (/backend branch).
    
    Co-Authored-By: thiennguyen00 <59074387+thiennguyen00@users.noreply.github.com>

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from app import db
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    role = db.Column(db.String(100))
+
+    def __init__(self, name, username, password, role):
+        self.name = name
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'username': self.username,
+            'password': self.password,
+            'role': self.role
+        }
---
+++ app/routes.py
@@ -0,0 +1,90 @@
+from app import app, db
+from app.models import Users
+from flask_cors import CORS, cross_origin
+from flask import jsonify
+from flask import request
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+import base64
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    exists = False
+    name = None
+    role = None
+
+    users = Users.query.all()
+    for row in users:
+        if(row.username == username and row.password == password):
+            exists = True
+            name= row.name
+            role = row.role
+            break
+
+    if exists == False:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    if role == 'Pending Approval':
+        return jsonify({"msg": "User has not been approved"}), 301
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token, name=name, role=role)
+
+@app.route("/create", methods=["POST"])
+def create():
+    name = request.json.get("name", None)
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    users = Users.query.all()
+    for row in users:
+        if(row.username == username):
+            return jsonify({"msg": "Username already exists"}), 410
+
+    db.session.add(Users(name=name,username=username,password=password, role='Pending Approval'))
+    db.session.commit()
+    return jsonify({"msg": "Success"}), 200
+
+
+
+@app.route("/users",methods=['GET'])
+@jwt_required()
+def sendUsers():
+    users = Users.query.order_by(Users.id.desc()).all()
+    respId = []
+    respName = []
+    respRole = []
+
+    for row in users:
+        respId.append(row.id)
+        respName.append(row.name)
+        respRole.append(row.role)
+        
+    return jsonify({"id": respId, "name": respName, "role": respRole})
+
+
+@app.route("/update", methods=["POST"])
+@jwt_required()
+def update():
+    id = request.json.get("id", None)
+    value = request.json.get("value", None)
+    user = Users.query.get(id)
+    user.role = value
+    db.session.commit()
+    return jsonify({"msg": "Success"}), 200
+
+
+@app.route("/delete", methods=["POST"])
+@jwt_required()
+def delete():
+    id = request.json.get("id", None)
+    duser = Users.query.get(id)
+    db.session.delete(duser)
+    db.session.commit()
+    return jsonify({"msg": "Success"}), 200
+
+    
